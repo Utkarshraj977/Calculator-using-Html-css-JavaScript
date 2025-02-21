a03c2c36186cb771996c380401da26ed26cc3dbc
utkarsh raj <utkarshbrb6@gmail.com>
2025-02-22 01:34:08 +0530

    Added calculator project successful

---
+++ app.js
@@ -0,0 +1,29 @@
+let input=document.getElementById('input-box');
+let buttons=document.querySelectorAll('button');
+
+let string='';
+let arr=Array.from(buttons);
+arr.forEach(button =>{
+    button.addEventListener('click',(e)=>{
+        if (e.target.innerHTML == '=') {
+            try {
+                let result = eval(string); 
+                input.value = "= " + result; 
+                string = result.toString(); 
+            } catch {
+                input.value = "Error"; 
+                string = ""; 
+            }
+        }else if(e.target.innerHTML=='AC'){
+            string='';
+            input.value=string;
+        }else if(e.target.innerHTML=='DEL'){
+            string=string.substring(0,string.length-1);
+            input.value=string;
+        }
+        else{
+            string+=e.target.innerHTML;
+            input.value=string; 
+        }
+    })
+})
